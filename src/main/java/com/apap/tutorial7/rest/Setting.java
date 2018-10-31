914563e47ab383ef8836bab7f89f948c1baf13dd
Ismail Al Ghani <ismailalghani@gmail.com>
2018-10-31 22:26:15 +0700

    Tutor Done, Latihan Done

--- src/main/java/com/apap/tutorial7/rest/Setting.java
+++ src/main/java/com/apap/tutorial7/rest/Setting.java
@@ -2,4 +2,5 @@ package com.apap.tutorial7.rest;
 
 public class Setting {
 	final public static String dealerUrl = "https://b066e72a-9008-443c-b15d-c7aee34cabdc.mock.pstmn.io";
+	final public static String modelUrl = "https://www.carqueryapi.com/api/0.3";
 }
